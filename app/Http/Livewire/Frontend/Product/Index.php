ee4548a4717513550890b4b167a6a23fd5c5a40a
Veasna <Veasna3d@gmail.com>
2023-06-01 16:53:02 +0700

    Filter

--- app/Http/Livewire/Frontend/Product/Index.php
+++ app/Http/Livewire/Frontend/Product/Index.php
@@ -2,21 +2,40 @@
 
 namespace App\Http\Livewire\Frontend\Product;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class Index extends Component
 {
     public $products;
     public $category;
+    public $brandInputs = [], $priceInputs;
 
-    public function mount($products, $category)
+    protected $queryString = [
+        'brandInputs' => ['except' => '', 'as' => 'brand'],
+        'priceInputs' => ['except' => '', 'as' => 'price']
+    ];
+
+    public function mount($category)
     {
-        $this->products = $products;
+
         $this->category = $category;
     }
 
     public function render()
     {
-        return view('livewire.frontend.product.index' ,['products' => $this->products, 'category' => $this->category]);
+        $this->products = Product::where('category_id', $this->category->id)
+        ->when($this->brandInputs, function ($q) {
+            $q->whereIn('brand', $this->brandInputs);
+        })->when($this->priceInputs, function ($q) {
+
+            $q->when($this->priceInputs == 'high-to-low', function ($q2) {
+                $q2->orderBy('selling_price', 'DESC');
+            })->when($this->priceInputs == 'low-to-high', function ($q2) {
+                $q2->orderBy('selling_price', 'ASC');
+            });
+
+        })->where('status', '0')->get();
+        return view('livewire.frontend.product.index', ['products' => $this->products, 'category' => $this->category]);
     }
 }
